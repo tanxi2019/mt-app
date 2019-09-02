522331bc96c5967d4467779cfdd46032d5ad6e25
unknown <1832355298@qq.com>
2019-09-02 23:18:18 +0800

    112

---
+++ components/public/header/topbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">topbar</div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <el-container>
+      <el-header>header</el-header>
+      <el-main>
+        <nuxt/>
+      </el-main>
+      <el-footer>footer</el-footer>
+    </el-container>
+</template>
+<script>
+export default {}
+</script>
