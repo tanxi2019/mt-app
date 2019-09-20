3dbe02e15d8e875a6464f6015ceb01af73345ea7
风很大 <1832355298@qq.com>
2019-09-20 09:06:06 +0800

    0000000000000000000

---
+++ pages/exit.vue
@@ -0,0 +1,16 @@
+<template>
+  <!--suppress CheckEmptyScriptTag -->
+  <div class="exit" />
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  middleware: async (ctx) => {
+    const { status, data } = await ctx.$axios.get('/users/exit')
+    if (status === 200 && data && data.code === 0) {
+      window.location.href = '/'
+    }
+  }
+}
+</script>
